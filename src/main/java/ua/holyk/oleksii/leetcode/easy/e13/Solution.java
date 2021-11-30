661ca683d590b0edf040a3e994c77c0cc4b9a5b6
Holyk, Oleksii <oleksii.holyk@McKesson.com>
2021-11-30 17:52:53 +0200

    [Task13] added structure for task #13

---
+++ src/main/java/ua/holyk/oleksii/leetcode/easy/e13/Solution.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright Oleksii Holyk(c) 2021.
+ */
+
+package ua.holyk.oleksii.leetcode.easy.e13;
+
+/*
+ * 13. Roman to Integer
+ * */
+public class Solution {
+    public int romanToInt(String s) {
+        return 0;
+    }
+}
---
+++ src/test/java/ua/holyk/oleksii/leetcode/easy/e13/TestSolution.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright Oleksii Holyk(c) 2021.
+ */
+
+package ua.holyk.oleksii.leetcode.easy.e13;
+
+public class TestSolution {
+
+}
